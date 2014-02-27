2fdce6ced49e358798f4d805186fddab123e082e
BioComSoftware <biocomsoftware@utr.net>
2014-02-27 18:14:22 +0100

    lic

--- HubzuAuctionsScraper/src/Copyright and licensing/2Clauses/Clause1.PERPETUAL_AND_UNLIMITED_LICENSING_TO_THE_CLIENT.py
+++ HubzuAuctionsScraper/src/Copyright and licensing/2Clauses/Clause1.PERPETUAL_AND_UNLIMITED_LICENSING_TO_THE_CLIENT.py
@@ -1,7 +1,8 @@
 """
 1. PERPETUAL AND UNLIMITED LICENSING TO THE CLIENT
-Any intellectual properties issued to the licensee (client) under the "BioCom 
-Perpetual License" are authorized for perpetual and unlimited use by the 
+Any intellectual properties issued to the licensee (client) under the 
+"PERPETUAL AND UNLIMITED LICENSING TO THE CLIENT" 
+are authorized for perpetual and unlimited use by the 
 licensee of said intellectual property including redistribution, copying, 
 modification and inclusion in other copyrighted intellectual property owned or 
 created by the licensee regardless as whether part of a non-profit or for-profit
