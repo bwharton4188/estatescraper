722d91232546c5e2aa4f19ed4a8ce55a1a37a77a
BioComSoftware <biocomsoftware@utr.net>
2014-02-27 15:12:29 +0100

    ..

--- HubzuAuctionsScraper/src/bin/estatescraper/pipelines.py
+++ HubzuAuctionsScraper/src/bin/estatescraper/pipelines.py
@@ -7,6 +7,7 @@ from csv import DictWriter
 
 class EstatescraperXLSwriter(object):
     def __init__(self):
+        print "Ive starter the __init__ in the pipeline" #333
 #         dispatcher.connect(self.spider_opened, signals.spider_opened)
 #         dispatcher.connect(self.spider_closed, signals.spider_closed)
         self.brandCategoryCsv = csv.writer(open('test.csv', 'wb'),
@@ -16,7 +17,9 @@ class EstatescraperXLSwriter(object):
 
     def open_spider(self, spider):
         pass
+    
     def process_item(self, item, spider):
+        print "attempting to run process_item" #333
         self.brandCategoryCsv.writerow([item['propertyID'],
                                         item['assetType']])
         return item
